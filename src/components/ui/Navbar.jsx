f21f72fb4d3ddd96bf2b348bcd914772e11bc23e
allenDG <allenwalterfiestada@gmail.com>
2024-10-07 15:39:02 +0800

    added navbar drawer

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,12 +1,17 @@
-import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Heading, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 import ThemeController from "./ThemeController";
 import NavbarList from "./NavbarList";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 export default function Navbar() {
   const bgColor = useColorModeValue("bg.light", "bg.dark");
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = useRef()
+
   return (
+   <>
     <Flex
       p="10px"
       justify="space-between"
@@ -31,7 +36,36 @@ export default function Navbar() {
           <NavbarList />
         </Flex>
         <ThemeController />
+         <Button display={{ base: "block", md: "none" }} ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button>
       </Flex>
     </Flex>
+
+    <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      
+      >
+        <DrawerOverlay  display={{ base: "block", md: "none" }}/>
+        <DrawerContent  display={{ base: "block", md: "none" }}>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+          <NavbarList />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+   </>
   );
 }
